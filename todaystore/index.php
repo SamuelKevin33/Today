03bc3c9901dd42fcdddc64c1d61bc57e5869872b
SamuelKevin33 <18302198262@163.com>
2018-05-23 16:44:00 +0800

    changemvc

--- todaystore/index.php
+++ todaystore/index.php
@@ -1,7 +1,8 @@
 <?php
 define("APP_PATH","../todaystoreApplication/");
 define('APP_DEBUG',true);
-// define('BIND_MODULE','Home');
-// define('BUILD_CONTROLLER_LIST','Index');
+define('BIND_MODULE','Home');
+define('BUILD_CONTROLLER_LIST','Index');
+define('BUILD_MODEL_LIST','Index');  
 require "../ThinkPHP/ThinkPHP.php";
 // echo "hi";

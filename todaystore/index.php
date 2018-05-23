7b82df22ebacbc0dac9f8cd2bd570de6b9214cea
SamuelKevin33 <18302198262@163.com>
2018-05-23 16:53:33 +0800

    lasttime

--- todaystore/index.php
+++ todaystore/index.php
@@ -2,7 +2,7 @@
 define("APP_PATH","../todaystoreApplication/");
 define('APP_DEBUG',true);
 // define('BIND_MODULE','Home');
-define('BUILD_CONTROLLER_LIST','Index');
-define('BUILD_MODEL_LIST','Index');  
+// define('BUILD_CONTROLLER_LIST','Index');
+// define('BUILD_MODEL_LIST','Index');  
 require "../ThinkPHP/ThinkPHP.php";
 // echo "hi";

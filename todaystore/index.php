abce57c65f366c3412bb16c619bad7b6244ed027
SamuelKevin33 <18302198262@163.com>
2018-05-23 15:43:33 +0800

    addcontroller

--- todaystore/index.php
+++ todaystore/index.php
@@ -2,5 +2,6 @@
 define("APP_PATH","../todaystoreApplication/");
 define('APP_DEBUG',true);
 define('BIND_MODULE','Home');
+define('BUILD_CONTROLLER_LIST','Index');
 require "../ThinkPHP/ThinkPHP.php";
 // echo "hi";
